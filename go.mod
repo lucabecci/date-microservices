6f3601b479aafbabca0f3276d751638a1d3df11d
luca <beccibrian@gmail.com>
2021-03-01 21:23:32 -0300

    chore(@all): Server and middlewares for server created

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/lucabecci/date-microservices
 
 go 1.15
 
-require github.com/go-kit/kit v0.10.0
+require (
+	github.com/go-kit/kit v0.10.0
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/prometheus/client_golang v1.3.0
+)
--- internal/server.go
+++ internal/server.go
@@ -1,1 +1,37 @@
 package internal
+
+import (
+	"context"
+	"net/http"
+
+	httptransport "github.com/go-kit/kit/transport/http"
+	"github.com/gorilla/mux"
+	"github.com/lucabecci/date-microservices/endpoints"
+	"github.com/lucabecci/date-microservices/internal/middlewares"
+	"github.com/lucabecci/date-microservices/utils"
+)
+
+func NewServer(ctx context.Context, endpoints endpoints.Endpoints) http.Handler {
+	r := mux.NewRouter()
+	r.Use(middlewares.CommonMiddleware)
+
+	r.Methods("GET").Path("/status").Handler(httptransport.NewServer(
+		endpoints.StatusEndpoint,
+		utils.DecodeStatusRequest,
+		utils.EncodeResponse,
+	))
+
+	r.Methods("GET").Path("/get").Handler(httptransport.NewServer(
+		endpoints.GetEndpoint,
+		utils.DecodeGetRequest,
+		utils.EncodeResponse,
+	))
+
+	r.Methods("POST").Path("/validate").Handler(httptransport.NewServer(
+		endpoints.ValidateEndpoint,
+		utils.DecodeValidateRequest,
+		utils.EncodeResponse,
+	))
+
+	return r
+}
