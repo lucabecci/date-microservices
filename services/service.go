e264067f75811e82efa94afbd764e4c3f60bc9cc
luca <beccibrian@gmail.com>
2021-03-01 21:55:03 -0300

    fix(@all): Fix in format value

--- services/service.go
+++ services/service.go
@@ -23,11 +23,11 @@ func (dateService) Status(ctx context.Context) (string, error) {
 
 func (dateService) Get(ctx context.Context) (string, error) {
 	now := time.Now()
-	return now.Format("22/10/2000"), nil
+	return now.Format("02/01/2006"), nil
 }
 
 func (dateService) Validate(ctx context.Context, date string) (bool, error) {
-	_, err := time.Parse("22/10/2000", date)
+	_, err := time.Parse("02/01/2006", date)
 	if err != nil {
 		return false, err
 	}
