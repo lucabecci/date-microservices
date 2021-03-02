e04cf369108d6a7cfbc38ded85da2ee36483cb75
luca <beccibrian@gmail.com>
2021-03-01 21:36:35 -0300

    chore(@all): Microservices finished and working

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"net/http"
+	"os"
 
-func main() { fmt.Println("Hello World") }
+	"github.com/go-kit/kit/log"
+	"github.com/lucabecci/date-microservices/endpoints"
+	"github.com/lucabecci/date-microservices/internal"
+	"github.com/lucabecci/date-microservices/services"
+)
+
+func main() {
+	logger := log.NewLogfmtLogger(os.Stderr)
+	ctx := context.Background()
+
+	srv := services.NewService()
+
+	endpoints := endpoints.Endpoints{
+		GetEndpoint:      endpoints.MakeGetEndpoint(srv),
+		StatusEndpoint:   endpoints.MakeStatusEndpoint(srv),
+		ValidateEndpoint: endpoints.MakeValidateEndpoint(srv),
+	}
+
+	handler := internal.NewServer(ctx, endpoints)
+	logger.Log("msg", "HTTP", "addr", ":4000")
+	logger.Log("err", http.ListenAndServe(":4000", handler))
+
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.15
 
 require (
 	github.com/go-kit/kit v0.10.0
-	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/mux v1.8.0
 	github.com/prometheus/client_golang v1.3.0
 )
